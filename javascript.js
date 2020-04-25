d630d830134b467f1f776db80d7d5423bb5acde7
Beston Keghtor <53708520+BestonKeghtor@users.noreply.github.com>
2020-04-25 09:25:55 -0700

    First Commit

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+    function validation(){
+        if (document.signIn.checkBox.checked)
+            submit_msg ();
+        else
+            alert("Please verify you are not a robot");
+    }
+   
+        function reset_warn () {
+            alert ("This will clear your information");
+           
+        }
+
